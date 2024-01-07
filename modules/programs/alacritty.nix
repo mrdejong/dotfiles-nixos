0d499a940dbd28b91fdc9c1cb320b7f5cd267433
Alexander de Jong <alexander@dutchcaffeine.nl>
2024-01-07 15:59:05 +0100

    Install alacritty

---
+++ modules/programs/alacritty.nix
@@ -0,0 +1,20 @@
+{ pkgs, vars, ... }:
+
+{
+  home-manager.users.${vars.user} = {
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        font = {
+          normal.family = "FiraCode Nerd Font";
+          bold = { style = "Bold"; };
+          size = 14;
+        };
+        offset = {
+          x = -1;
+          y = 0;
+        };
+      };
+    };
+  };
+}
